f361cbfe32ed6bee74604201fae8a62a0c097457
soroush-bn <soroush.baghernezhad3@gmail.com>
2023-03-03 00:41:51 +0330

    adding gui

--- Solution.py
+++ Solution.py
@@ -1,5 +1,8 @@
 from Problem import Problem
 from State import State
+import os
+
+GUI_ENABLE = True
 
 
 class Solution:
@@ -15,10 +18,26 @@ class Solution:
             state = state.parent
         print('Init State')
         self.problem.print_state(queue[0])
+        with open('.\\gui\\AI-GUI_Data\\StreamingAssets\\Test Input.txt', 'w') as file:
+            file.write(self.problem.get_state_for_gui(queue[0]))
+            file.close()
+        actions = ""
         for index, state in enumerate(queue[1:]):
             print('---------\n')
+            print('previous action : ' + str(state.prev_action) + "\n")
+            actions += 'P' + str(state.prev_action[0] + 1) + ',' + 'p' + str(state.prev_action[1] + 1) + '\n'
             print(f'next step => {index + 1}')
             self.problem.print_state(state)
+        with open('.\\gui\\AI-GUI_Data\\StreamingAssets\\Test Input.txt', 'a') as file:
+            file.write(actions)
+            file.close()
         print('---------\n')
         print('Solution State')
         self.problem.print_state(self.state)
+
+    def execute_gui(self):
+        absolute_path = os.path.dirname(__file__)
+        relative_path = "gui\AI-GUI.exe"
+        full_path = os.path.join(absolute_path, relative_path)
+        if GUI_ENABLE:
+            os.startfile(full_path)
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ if __name__ == '__main__':
     p1 = Pipe(['red', 'red', 'blue'], 4)
     p2 = Pipe(['blue', 'blue', 'blue'], 4)
     p3 = Pipe(['red', 'red'], 4)
+    p4 = Pipe(['blue', 'blue', 'blue'], 4)
+    p5 = Pipe(['blue'], 4)
 
-    s = Search.bfs(Problem(State([p1, p2, p3], None, 0)))
+    s = Search.bfs(Problem(State([p1, p2, p3, p4, p5], None, 0, (0, 0))))
     s.print_path()
+    s.execute_gui()
